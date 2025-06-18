3a847d4c59ef84ded1286cd57a9b3086d0166a22
Gab <gaboux22@gmail.com>
2025-06-18 00:41:16 -0400

    Auto push

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,34 +1,34 @@
-const fetch = require('node-fetch');
+const fetch = require("node-fetch");
 
 exports.handler = async function(event, context) {
   const itemId = event.queryStringParameters.itemId;
   const apiKey = event.queryStringParameters.key;
 
-  if (!itemId || !apiKey) {
+  if (!apiKey) {
     return {
       statusCode: 400,
-      body: JSON.stringify({ error: "Missing parameters." })
+      body: JSON.stringify({ error: "Missing Torn API key." })
     };
   }
 
-  const url = \`https://api.torn.com/v2/market/\${itemId}?selections=itemmarket&key=\${apiKey}\`;
+  const url = `https://api.torn.com/v2/market/${itemId}?selections=itemmarket&key=${apiKey}`;
 
   try {
-    const res = await fetch(url);
-    const json = await res.json();
+    const response = await fetch(url);
+    const json = await response.json();
 
     return {
       statusCode: 200,
+      body: JSON.stringify(json.data),
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json"
-      },
-      body: JSON.stringify(json.data)
+      }
     };
-  } catch (err) {
+  } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Erreur de proxy Torn." })
+      body: JSON.stringify({ error: "Erreur proxy Torn." })
     };
   }
 };
