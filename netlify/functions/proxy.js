7509cc43a6f63de6eff17fe6b43587c6761bbd77
Gab <gaboux22@gmail.com>
2025-06-18 11:33:45 -0400

    Auto push

--- netlify/functions/proxy.js
+++ netlify/functions/proxy.js
@@ -1,24 +1,18 @@
 
 const fetch = require('node-fetch');
 
-exports.handler = async function(event) {
+exports.handler = async function(event, context) {
   const itemId = event.queryStringParameters.itemId;
-  const apiKey = event.queryStringParameters.key;
+  const apiKey = "7SME3JWfIcVWRGLY";
+  const url = `https://api.torn.com/v2/market/${itemId}?selections=itemmarket&key=${apiKey}`;
 
-  if (!apiKey) {
-    return {
-      statusCode: 400,
-      body: JSON.stringify({ error: "Missing API key." })
-    };
-  }
-
-  const url = `https://api.torn.com/market/${itemId}?selections=market&key=${apiKey}`;
   try {
     const response = await fetch(url);
-    const json = await response.json();
+    const data = await response.json();
+
     return {
       statusCode: 200,
-      body: JSON.stringify({ item: json }),
+      body: JSON.stringify(data),
       headers: {
         "Access-Control-Allow-Origin": "*",
         "Content-Type": "application/json"
@@ -27,7 +21,7 @@ exports.handler = async function(event) {
   } catch (error) {
     return {
       statusCode: 500,
-      body: JSON.stringify({ error: "Erreur serveur Torn." })
+      body: JSON.stringify({ error: "Erreur proxy." })
     };
   }
 };
